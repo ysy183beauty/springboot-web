d4e97b140c27688c74aa6023235ad0b6f3798a1c
ysy_glb <ysy_glb@18388.com>
2019-10-17 16:52:32 +0800

    springboot-web second commit

--- src/main/java/com/springboot/config/MyMvcConfig.java
+++ src/main/java/com/springboot/config/MyMvcConfig.java
@@ -1,6 +1,9 @@
 package com.springboot.config;
 
+import com.springboot.compent.MyLocalResolver;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.LocaleResolver;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
@@ -10,5 +13,23 @@ public class MyMvcConfig extends WebMvcConfigurerAdapter {
     public void addViewControllers(ViewControllerRegistry registry) {
         //发送testHello请求，跳转到success
         registry.addViewController("/testHello").setViewName("success");
+    }
+
+    //所有的WebMvcConfigurerAdapter组件会起作用
+    @Bean
+    public WebMvcConfigurerAdapter webMvcConfigurerAdapter(){
+        WebMvcConfigurerAdapter adapter=new WebMvcConfigurerAdapter(){
+            @Override
+            public void addViewControllers(ViewControllerRegistry registry) {
+                registry.addViewController("/").setViewName("login");
+                registry.addViewController("/index.html").setViewName("login");
+            }
+        };
+        return adapter;
+    }
+
+    @Bean
+    public LocaleResolver localeResolver(){
+        return new MyLocalResolver();
     }
 }
