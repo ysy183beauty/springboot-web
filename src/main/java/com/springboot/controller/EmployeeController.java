435e8412585471493986c35ce3bf0c90e2bc3919
ysy_glb <ysy_glb@18388.com>
2019-10-20 17:23:14 +0800

    springboot-web five commit

--- src/main/java/com/springboot/controller/EmployeeController.java
+++ src/main/java/com/springboot/controller/EmployeeController.java
@@ -1,11 +1,16 @@
 package com.springboot.controller;
 
+import com.springboot.dao.DepartmentDao;
 import com.springboot.dao.EmployeeDao;
+import com.springboot.entities.Department;
 import com.springboot.entities.Employee;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 
 import java.util.Collection;
 
@@ -13,6 +18,9 @@ import java.util.Collection;
 public class EmployeeController {
     @Autowired
     EmployeeDao employeeDao;
+
+    @Autowired
+    DepartmentDao departmentDao;
 
     //查询所有员工列表
     @GetMapping("/emps")
@@ -22,5 +30,42 @@ public class EmployeeController {
         model.addAttribute("emps",emps);
         //路径classpath:/templates/xxx.html
       return "/emp/list";
+    }
+
+    //添加页面信息
+    @GetMapping("/emp")
+    public String toAddPage(Model model){
+        Collection<Department> departments = departmentDao.getDepartments();
+        model.addAttribute("depts",departments);
+        return "/emp/add";
+    }
+
+    //添加员工信息
+    //springMVC 将属性与字段一一对应，基本要求：请求参数名字与java Bean属性名称一致
+    @PostMapping("/emp")
+    public String addEmp(Employee employee){
+        //redirect:重定向一个新地址 /表示当前项目下
+        //forward:表示转发到一个地址
+        employeeDao.save(employee);
+        return "redirect:/emps";
+    }
+
+    //修改员工信息
+    @GetMapping("/emp/{id}")
+    public String toEditPage(@PathVariable("id") Integer id,Model model){
+       //通过id查询员工信息
+        Employee employee = employeeDao.get(id);
+        model.addAttribute("emp",employee);
+
+        //查询部门信息
+        Collection<Department> departments = departmentDao.getDepartments();
+        model.addAttribute("depts",departments);
+        return "emp/add";
+    }
+    //修改员工信息
+    @PutMapping("/emp")
+    public String updateEmployee(Employee employee){
+        employeeDao.save(employee);
+        return "redirect:/emps";
     }
 }
